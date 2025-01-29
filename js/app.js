58fd65654b90ce1c2451ca3c6ee45ef91e0c2b26
Nicolas Marquez <nicolas.marquezlima@gmail.com>
2025-01-29 15:18:10 -0300

    modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 //função comprar
 function comprar(){
     let ingresso = document.getElementById('tipo-ingresso').value;
-    let quantidade = document.getElementById('qtd').value;
+    let qtd = document.getElementById('qtd').value;
+    let qtdDisponivel = document.getElementById(`qtd-${ingresso}`).textContent;
+    valorSubtraido = qtdDisponivel - qtd;
     //verificação da validade da quantidade de ingressos selecionada
-    if (quantidade == '') {
-        alert('Informe uma quantidade válida!');
+    if (qtd == '' || valorSubtraido<0) {
+        alert('Informe uma quantidade válida e que ainda esteja disponivel!');
         return;
     } else {
-    alert (`Você selecionou ${quantidade} ingresso(s) do tipo ${ingresso}.`);
+        alert (`Você selecionou ${qtd} ingresso(s) do tipo ${ingresso}.`);
     }
     //subtrair da quantidade selecionada do total de ingressos
-    let qtdDisponivel = document.getElementById(`qtd-${ingresso}`).textContent;
-    valorSubtraido = qtdDisponivel - quantidade;
     document.getElementById(`qtd-${ingresso}`).innerHTML = valorSubtraido;
     alert (`Restam ${valorSubtraido} ingresso(s) do tipo ${ingresso}.`);
 }
